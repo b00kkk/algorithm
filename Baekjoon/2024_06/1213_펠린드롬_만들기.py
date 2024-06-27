111f9ba34706c626ac3b3c33cfb1814dce651458
Boogyeom Kim <153355571+qnrua0511@users.noreply.github.com>
2024-06-28 01:01:06 +0900

    Add files via upload

---
+++ Baekjoon/2024_06/1213_펠린드롬_만들기.py
@@ -0,0 +1,27 @@
+import sys
+a=sys.stdin.readline().strip()
+dic={}
+for i in a:
+    if i in dic:
+        dic[i]+=1
+    else:
+        dic[i]=1
+
+odd=0
+for key in dic:
+    if dic[key]%2!=0:
+        odd+=1
+
+if odd>1:
+    print("I'm Sorry Hansoo")
+else:
+    dic=sorted(dic.items())
+    x=''
+    y=''
+    for key,values in dic:
+        while values>=2:
+            x+=key
+            values-=2
+        if values==1:
+            y+=key
+    print(x+y+x[::-1])
