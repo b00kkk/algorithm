ea17f23500a027c6c8ee7a32baa88fb185a95102
qnrua0511 <qnrua0511@naver.com>
2024-06-29 14:37:01 +0900

    Create 0629_스택.py

---
+++ Baekjoon/2024_06/0629_스택.py
@@ -0,0 +1,26 @@
+#BOJ 1082
+import sys
+n=int(sys.stdin.readline())
+stack=[]
+for i in range(n):
+    a=sys.stdin.readline().rstrip()
+    if 'push' in a:
+        push,a_int=a.split()
+        stack.append(int(a_int))
+    elif a=='size':
+        print(len(stack))
+    elif a=='pop':
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif a=='top':
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack[-1])
+    elif a=='empty':
+        if len(stack)==0:
+            print(1)
+        else:
+            print(0)
